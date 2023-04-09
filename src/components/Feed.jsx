35c067be7ee0915ec1e040cfbd155ffbe97a06df
iamsandhyamaharjan <98277290+iamsandhyamaharjan@users.noreply.github.com>
2023-04-09 12:26:05 +0545

    header completed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+// import {Context } from "../context/contextApi"
+import LeftNav from "./LeftNav"
 
 export const Feed = () => {
   return (
-    <div>Feed</div>
+    <div className='flex flex-row h-[calc(100%-56px)]'>
+      <LeftNav/>
+    </div>
   )
 }
--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
-
-export const LeftNav = () => {
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import {LeftNavMenuItem} from "./LeftNavMenuItem"
+import {categories} from "../utils/constants"
+import {Context} from "../context/contextApi"
+ const LeftNav = () => {
   return (
-    <div>LeftNav</div>
+    <div className='md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240] md:translate-x-0 transition-all'>
+
+      <div className="flex px-5 flex-col" >
+        {
+          categories.map((item)=>{
+            return(
+
+             <>
+             <LeftNavMenuItem
+             text={item.type === "home" ? "Home" : item.name}
+             icon={item.icon}
+             />
+             </> 
+            )
+          })
+        }
+      </div>
+    </div>
   )
 }
+export default LeftNav
